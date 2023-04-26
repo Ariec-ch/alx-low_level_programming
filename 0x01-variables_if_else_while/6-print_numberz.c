aad0b69b9c3f0400cde54760c4e17ee84f30c975
Ariec-ch <charifaqs@gmail.com>
2023-04-26 12:15:46 -0700

    hreblih

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -  a program that prints the digital numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0 ; b < 10 ; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
