aa54a7d2916324fa2aa8addf96c2cd6fac16dac4
Ariec-ch <charifaqs@gmail.com>
2023-04-26 10:05:26 -0700

    as3ab lang

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,13 @@
 
 /**
  * main -  a program that prints the alphabet in lowercase
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
-
+{
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	return (0);
+}
