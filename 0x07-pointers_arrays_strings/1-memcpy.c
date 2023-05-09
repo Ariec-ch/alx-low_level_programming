73b28b3395cf69fb3fe95fe611a1de17615cc8dd
Ariec-ch <charifaqs@gmail.com>
2023-05-08 18:05:36 -0700

    c langf

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memcpy -  function copies n bytes from memory area src to memory area dest
+ * *_memcpy -  function copies n bytes from memory area src to memory area dest
  * @dest: character parameter
  * @src: character parameter
  * @n: input parameter
