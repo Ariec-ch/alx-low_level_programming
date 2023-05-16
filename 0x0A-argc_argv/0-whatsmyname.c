3d2dfcdaa56268e27f1db053eaf571d5810f4dc8
Ariec-ch <charifaqs@gmail.com>
2023-05-15 18:26:41 -0700

    okey

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: input
+ * @argv: character
+ * Return: 0 success
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+#!/bin/bash
+practice c lang
+argc argv
