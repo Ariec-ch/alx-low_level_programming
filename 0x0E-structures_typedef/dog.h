f8555229084fa952d42ac762e97d3aa578a66ad3
Ariec-ch <charifaqs@gmail.com>
2023-05-22 12:43:40 -0700

    ok

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct_dog - define a new type
+ * @name: character
+ * @age: float
+ * @owner: anoter char
+ * Description: a dog certificate
+ */
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
